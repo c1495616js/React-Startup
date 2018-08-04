642de46b6a0d665d2366238c4991438d489ddfc4
jerrywang761208 <c1495616@gmail.com>
2018-08-04 15:44:57 +0800

    react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import BasicExample from './react-router/test'
 const Index = () => {
-  return <div>Hello React!</div>;
+  return ( 
+    <div>
+      <p> Hello React! </p>
+      <BasicExample/>
+    </div>
+  );
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
